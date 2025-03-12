0225ca31b2ec9dc72aec8a360b3a849a5ee7da2d
jiinp2 <jiinpark.work@gmail.com>
2025-03-12 03:11:04 -0400

    Link app title in navbar to home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <header>
-      <h2>Focal</h2>
+      <h2>
+        <Link to="/">Focal</Link>
+      </h2>
     </header>
   );
 }
