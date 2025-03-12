4d8eea267bf494c41adadf4511ff9e4ff05a0560
jiinp2 <jiinpark.work@gmail.com>
2025-03-12 00:55:43 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Upload from "./pages/Upload/Upload";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={Home} />
+        <Route path={"/upload"} element={Upload} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
