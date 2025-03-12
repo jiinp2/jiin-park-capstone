328a25ab495a33d207f9bae984c4438df37cd75a
jiinp2 <jiinpark.work@gmail.com>
2025-03-12 13:29:02 -0400

    Add Dropzone component to Upload page

--- src/components/Dropzone/Dropzone.jsx
+++ src/components/Dropzone/Dropzone.jsx
@@ -2,16 +2,19 @@ import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import "./Dropzone.scss";
 
-function Dropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log(acceptedFiles);
-  }, []);
+function Dropzone({ onFilesSelected }) {
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      onFilesSelected(acceptedFiles);
+    },
+    [onFilesSelected]
+  );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <section>
-      <div {...getRootProps()}>
+    <section className="dropzone">
+      <div {...getRootProps()} className="dropzone__area">
         <input {...getInputProps()} />
         {isDragActive ? (
           <p>Drop your photos here</p>
--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,10 +1,20 @@
 import "./Upload.scss";
+import Dropzone from "../../components/Dropzone/Dropzone";
+import { useState } from "react";
 
 function Upload() {
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  const handleFilesSelected = (files) => {
+    setSelectedFiles(files);
+    console.log("Files recieved:", files);
+  };
+
   return (
     <section className="upload">
       <h1>Upload Your Photos</h1>
       <p>Start building your travel log by adding photos from your journey.</p>
+      <Dropzone onFilesSelected={handleFilesSelected} />
     </section>
   );
 }
