8f1f27e166aaf0aa5ccb6e7ba26f5707c2109dcd
jiinp2 <jiinpark.work@gmail.com>
2025-03-18 16:20:30 -0400

    Update to Upload and Navbar component

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -10,35 +10,6 @@ const Upload = () => {
   // Handle files from Dropzone
   const handleFilesSelected = (files) => {
     setSelectedFiles(files);
-    console.log("Files recieved:", files);
-  };
-
-  // Fetch metadata
-  const fetchMetadata = async (imagePaths) => {
-    if (!imagePaths || imagePaths.length === 0) {
-      console.log("No images provided.");
-      return;
-    }
-
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/metadata`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ imagePaths }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch metadata");
-      }
-
-      const { metadata } = await response.json();
-      console.log("Extracted metadata:", metadata);
-    } catch (error) {
-      console.error("Error fetching metadata:", error.message);
-    }
   };
 
   // Handle file upload
@@ -47,10 +18,14 @@ const Upload = () => {
       alert("No files selected.");
       return;
     }
+
     setUploading(true);
 
     const formData = new FormData();
-    selectedFiles.forEach((file) => {
+
+    // Converting files into an array
+    const filesArray = Array.from(selectedFiles);
+    filesArray.forEach((file) => {
       formData.append("images", file);
     });
 
@@ -68,8 +43,8 @@ const Upload = () => {
       }
 
       const { imagePaths } = await response.json();
+      console.log("Upload successful, stored images:", imagePaths);
 
-      await fetchMetadata(imagePaths);
       setSelectedFiles([]);
     } catch (error) {
       console.error("Upload error:", error);
