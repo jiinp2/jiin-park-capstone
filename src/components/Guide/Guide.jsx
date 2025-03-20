69fb044abe19535e6b200e4a28f73e612239cdf1
jiinp2 <jiinpark.work@gmail.com>
2025-03-20 00:16:59 -0400

    Create Guide component

---
+++ src/components/Guide/Guide.jsx
@@ -0,0 +1,14 @@
+import "./Guide.scss";
+
+function Guide() {
+  return (
+    <section className="guide">
+      <h2>Guide</h2>
+      <h3>Upload Your Photos</h3>
+      <h3>Visualize Your Journey</h3>
+      <h3>Save & Share</h3>
+    </section>
+  );
+}
+
+export default Guide;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.scss";
 import Hero from "../../components/Hero/Hero";
+import Guide from "../../components/Guide/Guide";
 
 function Home() {
   return (
     <section>
       <Hero />
+      <Guide />
     </section>
   );
 }
