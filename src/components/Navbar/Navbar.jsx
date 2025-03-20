12b0e3f14bcc50c51f867818aa731a6b7c1821a4
jiinp2 <jiinpark.work@gmail.com>
2025-03-19 20:29:40 -0400

    Adjust margin mixin to only apply horizontally

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,10 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
-import logo from "../../assets/icons/focal-logo.svg";
 
 function Navbar() {
   return (
     <header>
       <Link to="/" className="header">
-        <img src={logo} alt="Logo" className="header__logo" />
         <h2>Focal</h2>
       </Link>
     </header>
