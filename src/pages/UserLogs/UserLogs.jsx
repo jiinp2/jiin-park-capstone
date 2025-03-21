18ed3bfe5252dc11210c302b6658fe62c6fcad60
jiinp2 <jiinpark.work@gmail.com>
2025-03-21 17:19:51 -0400

    Fetch saved logs

--- src/pages/UserLogs/UserLogs.jsx
+++ src/pages/UserLogs/UserLogs.jsx
@@ -1,1 +1,29 @@
+import { useEffect, useState } from "react";
 import "./UserLogs.scss";
+
+const UserLogs = () => {
+  const navigate = useNavigate();
+  const [logs, setLogs] = useState([]);
+
+  // Fetch saved logs
+  useEffect(() => {
+    const fetchLogs = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/logs`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch logs");
+        }
+
+        const data = await response.json();
+        setLogs(data.logs);
+      } catch (error) {
+        console.error("Error fetching logs:", error);
+      }
+    };
+    fetchLogs();
+  }, []);
+};
+
+export default UserLogs;
